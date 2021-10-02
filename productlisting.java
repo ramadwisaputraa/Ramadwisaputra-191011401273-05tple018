0452b86334708ae3282a3b665dc0fd255c67dcdf
ramadwisaputraa <91785862+ramadwisaputraa@users.noreply.github.com>
2021-10-02 19:28:59 +0700

    Add files via upload

---
+++ productlisting.java
@@ -0,0 +1,10 @@
+package produk;
+//nama : Rama Dwi Saputra
+//Nim  : 191011401273
+
+public class productListing {
+    public static void main(String[] args) {
+        product product_a = new product("product a");
+        product_a.getProductListing(5, 15, 10);
+    }
+}
